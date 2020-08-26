5106f4b72e337cf3de11c87c2ddc76f18ab712b7
soukaryag <sg4fz@virginia.edu>
2020-08-26 00:16:54 -0400

    abstract IP and add options to spinning up server

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from HttpServer import HttpServer
+import socket
 
 if __name__ == "__main__":
-    server = HttpServer(bind_ip="192.168.1.167")
+    IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+    LOCALHOST = "127.0.0.1"
+
+    print("Do you want to run this on localhost or your IP?")
+    print("[1] My IP -", IP_ADDRESS + ":8000")
+    print("[2] localhost -", LOCALHOST + ":8000")
+
+    choice = input()
+    if choice == "1":
+        BIND_IP = IP_ADDRESS
+        
+    elif choice == "2":
+        BIND_IP = LOCALHOST
+    else:
+        print("That is not a valid option!")
+
+    server = HttpServer(bind_ip=BIND_IP)
     server.start()
